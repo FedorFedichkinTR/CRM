fd5ca47a6bbf62fdf08d505a24bd3a9865a045d6
FedorFedichkin <f.fedichkin@gmail.com>
2021-07-09 15:47:50 +0300

    First commit

---
+++ src/test/java/com/time/to/shop/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.to.shop.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
