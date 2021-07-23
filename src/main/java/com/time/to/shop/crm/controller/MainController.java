9e6abc64d60c9cd83ef8ed2fc11e47c4dabe7e1e
FedorFedichkin <f.fedichkin@gmail.com>
2021-07-23 17:47:37 +0300

    added index.html

--- src/main/java/com/time/to/shop/crm/controller/MainController.java
+++ src/main/java/com/time/to/shop/crm/controller/MainController.java
@@ -1,13 +1,14 @@
 package com.time.to.shop.crm.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello!";
+        return "index";
     }
 }
