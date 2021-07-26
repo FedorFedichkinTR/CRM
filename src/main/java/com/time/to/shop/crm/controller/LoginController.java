8aa258cf636e8fc717aef58ef018859fb6c45c4e
FedorFedichkin <f.fedichkin@gmail.com>
2021-07-26 19:27:10 +0300

    minor correction

--- src/main/java/com/time/to/shop/crm/controller/LoginController.java
+++ src/main/java/com/time/to/shop/crm/controller/LoginController.java
@@ -1,14 +1,8 @@
 package com.time.to.shop.crm.controller;
 
-import com.time.to.shop.crm.model.db.User;
 import com.time.to.shop.crm.model.dto.LoginDTO;
 import com.time.to.shop.crm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +23,6 @@ public class LoginController {
     @PostMapping(value = "/login")
     public String login(@ModelAttribute(name = "loginDTO") @Valid LoginDTO loginDTO, Model model) {
         if (userService.isUserPresentInDB(loginDTO)) {
-            System.out.println("logged in");
             return "main";
         } else {
             model.addAttribute("invalidCredentials", true);
