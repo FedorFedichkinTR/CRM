bb879652b1250abd0a044be739449b8cb10b2215
FedorFedichkin <f.fedichkin@gmail.com>
2021-07-28 01:20:19 +0300

    Order entity created

--- src/main/java/com/time/to/shop/crm/model/db/Order.java
+++ src/main/java/com/time/to/shop/crm/model/db/Order.java
@@ -1,5 +1,34 @@
 package com.time.to.shop.crm.model.db;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orders_seq")
+    @SequenceGenerator(name = "orders_seq", sequenceName = "orders_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private Integer sellPrice;
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Long customerId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date sellDate;
+    private String notes;
 
 }
